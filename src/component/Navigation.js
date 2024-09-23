ed3b399a10bcb0f7e2fed5c55334d92d30c7a58e
Dhruvan687 <dhruvangandhi687@gmail.com>
2024-09-23 17:49:18 -0400

    change navigationbar with responsive design

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import '../App.css'
+import React, { useEffect } from 'react';
+import '../App.css';
 
 const Navigation = () => {
   const handleCollapse = () => {
@@ -8,172 +8,130 @@ const Navigation = () => {
     document.getElementById('showDrop').checked = false;
   };
 
+  useEffect(() => {
+    const toggle = document.getElementById('nav-toggle');
+    const nav = document.getElementById('nav-menu');
+
+    if (toggle && nav) {
+      toggle.addEventListener('click', () => {
+        nav.classList.toggle('show-menu');
+      });
+    }
+
+    // Cleanup event listener on component unmount
+    return () => {
+      if (toggle) {
+        toggle.removeEventListener('click', () => {
+          nav.classList.toggle('show-menu');
+        });
+      }
+    };
+  }, []);
+
   return (
-    <nav className="Main-Nav">
-      <div className="Nav-Wrapper">
-        <div className="logo">
-          <img src="assets/images/logo.png" alt="Site Logo" />
-        </div>
-        <input type="checkbox" name="nav-slide" id="button-menu" />
-        <input type="checkbox" name="nav-slide" id="button-menu" />
-        <ul className="Nav-Links">
-          <label
-            htmlFor="button-menu"
-            className="toggle-button-nav button-cancel"
-            onClick={handleCollapse}
-          >
-            <i className="fi fi-rr-cross"></i>
-          </label>
-          <li>
-            <a href="/" onClick={handleCollapse}>INTERNSHIP</a>
-          </li>
-          <li>
-            <a href="/products" className="for-desktop" onClick={handleCollapse}>
-              SKILLS
-            </a>
-            <input type="checkbox" id="showMenu" />
-            <label htmlFor="showMenu" className="for-mobile" onClick={handleCollapse}>
-              SKILLS
-            </label>
-            <div className="Mega-Box">
-              <div className="Mega-Box-Content">
-                <div className="Content-Row">
-                  <img src="assets/images/istockphoto-1166954025-170667a-removebg-preview.png" />
-                </div>
-                <div className="Content-Row">
-                  <header>Front-End</header>
-                  <ul className="Mega-Links">
-                    <li>
-                      <a href="#" onClick={handleCollapse}>React Js</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Javacript</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>HTML5</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Css3</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Bootstrap</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Media-Query</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Webpack</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Swagger UI</a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="Content-Row">
-                  <header>UX-UI</header>
-                  <ul className="Mega-Links">
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Adobe XD</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Figma</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Sketch</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Adobe AI</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Adobe Photoshop</a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="Content-Row">
-                  <header>Back-End</header>
-                  <ul className="Mega-Links">
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Node.js</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Fast API</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>REST APIs</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Flask</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Django</a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="Content-Row">
-                  <header>Databases</header>
-                  <ul className="Mega-Links">
-                    <li>
-                      <a href="#" onClick={handleCollapse}>MySQL</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>PostgreSQL</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>PL/SQL</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>MongoDB</a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={handleCollapse}>Firebase</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li>
-            <a href="/" onClick={handleCollapse}>blog</a>
-          </li>
-          <li>
-            <a href="/contact-us" onClick={handleCollapse}>Contact Us</a>
-          </li>
-          <li>
-            <a href="/" onClick={handleCollapse}>why us?</a>
-          </li>
-          <li>
-            <a href="#" className="for-desktop" onClick={handleCollapse}>
-              free courses
-            </a>
-            <input type="checkbox" id="showDrop" />
-            <label htmlFor="showDrop" className="for-mobile" onClick={handleCollapse}>
-              free courses
-            </label>
-            <ul className="Drop-Down">
-              <li>
-                <a href="/about-us" onClick={handleCollapse}>HTML CSS</a>
-              </li>
-              <li>
-                <a href="#" onClick={handleCollapse}>Programming fundamentals</a>
-              </li>
-              <li>
-                <a href="#" onClick={handleCollapse}>JQuery</a>
-              </li>
-              <li>
-                <a href="#" onClick={handleCollapse}>Responsive Design</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-        <label
-          htmlFor="button-menu"
-          className="toggle-button-nav button-menu"
-          onClick={handleCollapse}
-        >
-          <i className="fi fi-rr-align-justify"></i>
-        </label>
-      </div>
-    </nav>
+    // <!--=============== HEADER ===============-->
+    <header className="header">
+       <nav className="nav container">
+          <div className="nav__data">
+             <a href="#" className="nav__logo">
+                <i className="ri-planet-line"></i> Portfolio
+             </a>
+             
+             <div className="nav__toggle" id="nav-toggle">
+                <i className="ri-menu-line nav__burger"></i>
+                <i className="ri-close-line nav__close"></i>
+             </div>
+          </div>
+
+          {/* <!--=============== NAV MENU ===============--> */}
+          <div className="nav__menu" id="nav-menu">
+             <ul className="nav__list">
+                <li><a href="#" className="nav__link">Home</a></li>
+
+                <li><a href="#" className="nav__link">Company</a></li>
+
+                {/* <!--=============== DROPDOWN 1 ===============--> */}
+                <li className="dropdown__item">
+                   <div className="nav__link">
+                      Analytics <i className="ri-arrow-down-s-line dropdown__arrow"></i>
+                   </div>
+
+                   <ul className="dropdown__menu">
+                      <li>
+                         <a href="#" className="dropdown__link">
+                            <i className="ri-pie-chart-line"></i> Overview
+                         </a>                          
+                      </li>
+
+                      <li>
+                         <a href="#" className="dropdown__link">
+                            <i className="ri-arrow-up-down-line"></i> Transactions
+                         </a>
+                      </li>
+
+                      {/* <!--=============== DROPDOWN SUBMENU ===============--> */}
+                      <li className="dropdown__subitem">
+                         <div className="dropdown__link">
+                            <i className="ri-bar-chart-line"></i> Reports <i className="ri-add-line dropdown__add"></i>
+                         </div>
+
+                         <ul className="dropdown__submenu">
+                            <li>
+                               <a href="#" className="dropdown__sublink">
+                                  <i className="ri-file-list-line"></i> Documents
+                               </a>
+                            </li>
+    
+                            <li>
+                               <a href="#" className="dropdown__sublink">
+                                  <i className="ri-cash-line"></i> Payments
+                               </a>
+                            </li>
+    
+                            <li>
+                               <a href="#" className="dropdown__sublink">
+                                  <i className="ri-refund-2-line"></i> Refunds
+                               </a>
+                            </li>
+                         </ul>
+                      </li>
+                   </ul>
+                </li>
+                
+                <li><a href="#" className="nav__link">Products</a></li>
+
+                {/* <!--=============== DROPDOWN 2 ===============--> */}
+                <li className="dropdown__item">
+                   <div className="nav__link">
+                      Users <i className="ri-arrow-down-s-line dropdown__arrow"></i>
+                   </div>
+
+                   <ul className="dropdown__menu">
+                      <li>
+                         <a href="#" className="dropdown__link">
+                            <i className="ri-user-line"></i> Profiles
+                         </a>                          
+                      </li>
+
+                      <li>
+                         <a href="#" className="dropdown__link">
+                            <i className="ri-lock-line"></i> Accounts
+                         </a>
+                      </li>
+
+                      <li>
+                         <a href="#" className="dropdown__link">
+                            <i className="ri-message-3-line"></i> Messages
+                         </a>
+                      </li>
+                   </ul>
+                </li>
+
+                <li><a href="#" className="nav__link">Contact</a></li>
+             </ul>
+          </div>
+       </nav>
+    </header>
   );
 };
 
